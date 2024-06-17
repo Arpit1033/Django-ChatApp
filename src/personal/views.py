a471c9a9c8d3a10aafb1f0d352a488db305d75f5
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-18 01:46:38 +0530

    Made basic home page view.

--- src/personal/views.py
+++ src/personal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_screen_view(request, *args, **kwargs):
+    context = {}
+    return render(request, "personal/home.html",context)
