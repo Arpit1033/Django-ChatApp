20ce9b8b3874c1dfd29a780ce310f5e12f3be7af
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-18 01:59:34 +0530

    Added boostarp css and js files for styling with boostrap. Made changes to settings.py file and urls.py files after adding "static" and "media" folders to the project.

--- src/ChatServerPlayground/settings.py
+++ src/ChatServerPlayground/settings.py
@@ -133,4 +133,16 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+]
+
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
+
+TEMP = os.path.join(BASE_DIR, "media_cdn/temp")
+
+BASE_DIR = "http://127.0.0.1:8000"
