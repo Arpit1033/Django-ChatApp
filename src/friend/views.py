1eb8ccf47a9de6fb0f99d1180f90f0199d6fc195
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-18 18:37:33 +0530

    Update admin.py and models.py in the friend app. Added logic for friend system in view.py

--- src/friend/views.py
+++ src/friend/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from friend.models import FriendRequest, FriendList
 
 # Create your views here.
+def remove_friend(request, *args, **kwargs):
+	user = request.user
+	payload = {}
+	if request.method == "POST" and user.is_authenticated:
+		user_id = request.POST.get("receiver_user_id")
+		if user_id:
+			try:
+				removee = Account.objects.get(pk=user_id)
+				friend_list = FriendList.objects.get(user=user)
+				friend_list.unfriend(removee)
+				payload['response'] = "Successfully removed that friend."
+			except Exception as e:
+				payload['response'] = f"Something went wrong: {str(e)}"
+		else:
+			payload['response'] = "There was an error. Unable to remove that friend."
+	else:
+		# should never happen
+		payload['response'] = "You must be authenticated to remove a friend."
+	return HttpResponse(json.dumps(payload), content_type="application/json")
