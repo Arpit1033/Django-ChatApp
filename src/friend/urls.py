db64fb1df52d16b583a815622e00c9eee346cc49
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-18 19:38:30 +0530

    Created a url for accepting the friend request.

--- src/friend/urls.py
+++ src/friend/urls.py
@@ -6,4 +6,5 @@ app_name = 'friend'
 urlpatterns = [
     path('friend_request/', send_friend_request, name='friend-request'),
     path('friend_requests/<user_id>/', friend_requests, name='friend-requests'),
+    path('friend_request_accept/<friend_request_id>/', accept_friend_request, name='friend-request-accept'),
 ]
