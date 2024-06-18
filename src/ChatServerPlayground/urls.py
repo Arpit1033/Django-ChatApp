40bbb548fc72df4d2129389e4df2fa49e88d8651
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-18 10:33:24 +0530

    Update urls.py and header.html for user registration functionality

--- src/ChatServerPlayground/urls.py
+++ src/ChatServerPlayground/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from account.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("personal.urls")),
+    path('register/', register_view, name="register"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
