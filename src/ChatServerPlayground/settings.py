1c9b5b0380c3e670c0e5b8151becb2d309faa09c
Arpit1033 <lcb2022015@iiitl.ac.in>
2024-06-18 13:33:26 +0530

    Adding functionality for editing the profile of the user.

--- src/ChatServerPlayground/settings.py
+++ src/ChatServerPlayground/settings.py
@@ -153,3 +153,5 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     
 BASE_URL = "http://127.0.0.1:8000"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10mb = 10 * 1024 *1024
--- src/account/urls.py
+++ src/account/urls.py
@@ -5,4 +5,5 @@ app_name = 'account'
 
 urlpatterns = [
 	path('<user_id>/', account_view, name="view"),
+ 	path('<user_id>/edit/', edit_account_view, name="edit"),
 ]
